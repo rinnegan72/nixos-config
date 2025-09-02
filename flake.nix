40114560df77506cb34f18ba99bdbb5fcf923c67
Sriram Ramesh <sriram.ramesh312@gmail.com>
2025-09-02 23:06:33 +0530

    updated flake to include new package

--- flake.nix
+++ flake.nix
@@ -4,24 +4,38 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    vicinae.url = "github:vicinaehq/vicinae";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { 
-          inherit inputs;
-          unstable = import nixpkgs-unstable {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, vicinae }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { 
+            inherit inputs;
+            unstable = import nixpkgs-unstable {
+              system = "x86_64-linux";
+              config.allowUnfree = true;
+            };
           };
+          modules = [
+            ./configuration.nix
+            ./hardware-configuration.nix
+          ];
         };
+      };
+      
+      homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
         modules = [
-          ./configuration.nix
-          ./hardware-configuration.nix
+          vicinae.homeManagerModules.default
         ];
       };
     };
-  };
 }
